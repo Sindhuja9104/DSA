edcf12eacdbfe35bb326aabb818cd0759f139923
Sindhuja Bommu <bommusindhuja2@gmail.com>
2026-02-23 13:09:06 +0530

    Add files via upload

---
+++ ratInMazeFourDirection.java
@@ -0,0 +1,28 @@
+public class  ratInMazeFourDirection{
+    public static void maze(int sr, int sc, int er, int ec,String s,boolean[][] isVisited){
+        //base ase
+        if(sr < 0 || sc <0)return;
+        if(sr > er || sc > er)return;
+        if(isVisited[sr][sc] == true)return;
+        if(sr == er && sc == ec){
+            System.out.println(s);
+            return;
+        }
+        isVisited[sr][sc]=true;
+        //rigth way
+        maze(sr, sc+1, er, ec,s+"R",isVisited);
+        //left way
+        maze(sr, sc-1, er, ec, s+"L", isVisited);
+        //down
+        maze(sr+1, sc, er, ec, s+"D", isVisited);
+        //up
+        maze(sr-1, sc, er, ec, s+"U", isVisited);
+        isVisited[sr][sc] = false;
+    }
+    public static void main(String[] args){
+        int row = 2;
+        int col = 2;
+        boolean[][]  isVisited = new boolean[row][col];
+        maze(0,0,row-1,col-1,"",isVisited);
+    }
+} 
